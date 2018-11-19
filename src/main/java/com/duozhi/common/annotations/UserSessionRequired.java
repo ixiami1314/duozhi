f499e51b28ac405910ff4f33506650ec8978bc4e
weijun <2059746@qq.com>
2018-11-19 20:36:34 +0800

    '初始化develop版本'

---
+++ src/main/java/com/duozhi/common/annotations/UserSessionRequired.java
@@ -0,0 +1,12 @@
+package com.duozhi.common.annotations;
+
+/**
+ * 应用模块名称<p>
+ * 代码描述<p>
+ * Copyright: Copyright (C) 2018 Meijiequan.com, Inc. All rights reserved. <p>
+ * Company: 研福网络<p>
+ * @author weijun
+ * @since 2018/11/19 20:32 
+ */
+public @interface UserSessionRequired {
+}
---
+++ src/main/java/com/duozhi/utils/MathUtils.java
@@ -0,0 +1,12 @@
+package com.duozhi.utils;
+
+/**
+ * 应用模块名称<p>
+ * 代码描述<p>
+ * Copyright: Copyright (C) 2018 Meijiequan.com, Inc. All rights reserved. <p>
+ * Company: 研福网络<p>
+ * @author weijun
+ * @since 2018/11/19 20:33 
+ */
+public class MathUtils {
+}
